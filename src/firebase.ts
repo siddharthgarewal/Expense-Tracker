d5ded9398fe51cf57a1932a2f03155f9c2f9c55e
Siddharth Garewal <sidgarewal@gmail.com>
2024-03-09 21:03:25 +0530

    feat: firebase setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfy_U_pIyXnOnmY8G4THBvGubWrznNGH0",
-  authDomain: "expense-tracker-70e15.firebaseapp.com",
-  projectId: "expense-tracker-70e15",
-  storageBucket: "expense-tracker-70e15.appspot.com",
-  messagingSenderId: "670097043078",
-  appId: "1:670097043078:web:ee70a48f107ae000976b07",
-  measurementId: "G-4RNB661QDD",
+  apiKey: "AIzaSyDCpurV4hAQVpdTnshTz_Q2Tfs6y-N3-ZU",
+  authDomain: "expense-tracker-management.firebaseapp.com",
+  projectId: "expense-tracker-management",
+  storageBucket: "expense-tracker-management.appspot.com",
+  messagingSenderId: "775278203000",
+  appId: "1:775278203000:web:efc42a965b83fca1eadd8d",
+  measurementId: "G-BKP8BLFHGJ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-export const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
