42785936adf8330de789e6d4fb846d3b55fcc3d3
Anjali Garewal <anjaligarewal15@gmail.com>
2024-03-13 21:02:26 +0530

    testing initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import ExpenseForm from "./components/add-expense-form/ExpenseForm";
 import Header from "./components/header/Header";
+import Test from "./components/test/Test";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="subpart">
-        <ExpenseForm />
+        <Test />
+        {/* <ExpenseForm /> */}
       </div>
     </div>
   );
