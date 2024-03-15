1c4f9ca28995bc0c61f353bf6bf4aa9a070ca752
Siddharth Garewal <sidgarewal@gmail.com>
2024-03-15 18:49:27 +0530

    feat: firebase store setup

--- src/components/test/Test.tsx
+++ src/components/test/Test.tsx
@@ -1,7 +1,44 @@
+import { addDoc, collection, getDocs } from "@firebase/firestore";
+import { db } from "../../firebase";
+import { useState } from "react";
+
 function Test() {
+  const [data, setData] = useState("");
+
+  const addData = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "test"), { testing: data });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      await getDocs(collection(db, "test")).then((querySnapshot) => {
+        const newData = querySnapshot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        console.log(newData);
+      });
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
   return (
-    <div>Test 1</div>
-  )
+    <div>
+      <input
+        value={data}
+        type="text"
+        onChange={(event: any) => setData(event.target.value)}
+      />
+      <button onClick={() => addData()}>Save Data</button>
+      <button onClick={() => getData()}>Get Data</button>
+    </div>
+  );
 }
 
-export default Test
+export default Test;
